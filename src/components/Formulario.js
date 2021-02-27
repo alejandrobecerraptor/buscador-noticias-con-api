08ae852e296d433e7503c394f6324954c20229ba
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-27 14:19:42 -0500

    se agrega la documentacion, los proptypes a los componentes

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
+import PropTypes from 'prop-types';
 
 const Formulario = ({guardarCategoria}) => {
 
@@ -43,6 +44,10 @@ const Formulario = ({guardarCategoria}) => {
             </div>
         </div>
      );
+}
+
+Formulario.propTypes = {
+    guardarCategoria: PropTypes.func.isRequired
 }
  
 export default Formulario;
